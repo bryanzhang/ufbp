e4fb25a3b51710167eebe6ae4cd2b7470c7f4c34
junhao.zhang <junhao.zhang@dianping.com>
2014-12-21 15:20:37 +0800

    Add TransferStateQueue.

---
+++ transfer_state_queue.hpp
@@ -0,0 +1,37 @@
+#ifndef TRANSFER_STATE_QUEUE_HPP_
+#define TRANSFER_STATE_QUEUE_HPP_
+
+#include "wait_ack_queue.hpp"
+
+struct TransferState {
+  int socket;
+  int sendPos;
+  long lastSendTime;
+  long lastAckTime;
+  WaitAckQueue waitAckQueue;
+
+  TransferState(int s, unsigned long t) : socket(s), sendPos(0), lastSendTime(t), lastAckTime(t) {
+  }
+};
+
+struct TransferStateCompartor {
+  bool operator() (TransferState*& lhs, TransferState*& rhs) {
+    if (lhs->lastSendTime < rhs->lastSendTime) {
+      return true;
+    } else if (lhs->lastSendTime == rhs->lastSendTime) {
+      return lhs->lastAckTime < rhs->lastAckTime;
+    } else {
+      return false;
+    }
+  }
+};
+
+struct TransferStateFinalizer {
+  bool operator() (TransferState* state) {
+    delete state;
+  }
+};
+
+typedef HashedPriorityQueue<int, TransferState*, TransferStateCompartor, TransferStateFinalizer> TransferStateQueue;
+
+#endif  // TRANSFER_STATE_QUEUE_HPP_
---
+++ transfer_state_queue_test.cc
@@ -0,0 +1,5 @@
+#include "transfer_state_queue.hpp"
+
+int main(int argc, char** argv) {
+  TransferStateQueue queue; 
+}
